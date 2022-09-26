747785589996892c8c6802264ee1b236bbdc3619
John Burns <johnb1003@gmail.com>
2022-09-26 11:28:10 -0600

    Spring Initializr

---
+++ src/main/java/com/eatthefrog/UserProvisioningService/UserProvisioningServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eatthefrog.UserProvisioningService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProvisioningServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProvisioningServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eatthefrog/UserProvisioningService/UserProvisioningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatthefrog.UserProvisioningService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProvisioningServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

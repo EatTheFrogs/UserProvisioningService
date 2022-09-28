8258e4f34d9a7788f2fc1b05215dd54a3dd7d867
John Burns <johnburnsdev@gmail.com>
2022-09-28 17:32:34 -0600

    Revert "Add Jenkinsfile and Okta verification"

--- src/main/java/com/eatthefrog/UserProvisioningService/UserProvisioningServiceApplication.java
+++ src/main/java/com/eatthefrog/UserProvisioningService/UserProvisioningServiceApplication.java
@@ -1,9 +1,7 @@
 package com.eatthefrog.UserProvisioningService;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserProvisioningServiceApplication {
@@ -12,8 +10,4 @@ public class UserProvisioningServiceApplication {
 		SpringApplication.run(UserProvisioningServiceApplication.class, args);
 	}
 
-	@Bean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
-	}
 }
